a4fa2daec2d969a7264c1f109eedc06819bdf8ac
Phan Duc <38982972+Gumball1506@users.noreply.github.com>
2019-12-14 17:42:29 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,4 +2,4 @@ LOCAL_PATH := device/samsung/fortuna3g
 PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/rr_fortuna3g.mk \
 	$(LOCAL_DIR)/pa_fortuna3g.mk \
-	$(LOCAL_DIR)/lineage_fortuna3g.mk
+	$(LOCAL_DIR)/aosp_fortuna3g.mk
